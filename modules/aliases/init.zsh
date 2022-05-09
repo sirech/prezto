121ba95cecca3fc6c8a842be539b36e348598fbe
Mario Fernandez <mario.fernandez@hceris.com>
2022-05-09 20:27:01 +0200

    add extra aliases

--- modules/aliases/init.zsh
+++ modules/aliases/init.zsh
@@ -9,3 +9,17 @@ alias gu='git pull -v'
 alias ga='git add -v'
 alias gd='git diff'
 alias gl='git lg'
+
+# Global aliases
+alias -g C='| wc -l'
+alias -g H='| head'
+alias -g L="| less"
+alias -g N="| /dev/null"
+alias -g S='| sort'
+alias -g G='| grep' # now you can do: ls foo G something
+alias -g F='| fzf'
+
+# exa
+if test $(which exa); then
+  alias l='exa -lah'
+fi
