cb484513e085d3a080d8f185e287df85e54ccf2c
Mario Fernandez <mario.fernandez@hceris.com>
2022-05-09 18:53:06 +0200

    git config

--- modules/aliases/init.zsh
+++ modules/aliases/init.zsh
@@ -2,3 +2,10 @@
 alias h='history'
 alias hs='history | grep'
 alias hsi='history | grep -i'
+
+# git
+alias gs='git status'
+alias gu='git pull -v'
+alias ga='git add -v'
+alias gd='git diff'
+alias gl='git lg'
